d81a7e54ffd0e33c329578ba2dc614b17ab90cda
FernandaSolis <fersolis518@gmail.com>
2021-06-13 20:23:34 -0500

    header styles

--- harrypotter/src/App.js
+++ harrypotter/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+
+import './scss/bootstrap.scss';
 import './scss/app.scss';
+import Logo from './img/HarryPotter.png'
 
 function App() {
   return (
-    <div className="App">
-      <p>Hola</p>
+    <div className='App'>
+      <div className='header'>
+      <img src={Logo} className='logo' alt=''/>
+      <p className='firstText'>Selecciona tu filtro</p>
+      </div>
     </div>
   );
 }
