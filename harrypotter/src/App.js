cb04bf4b06f348edf8dd87b608001690a520dce1
FernandaSolis <fersolis518@gmail.com>
2021-06-14 17:40:23 -0500

    add character with json-server

--- harrypotter/src/App.js
+++ harrypotter/src/App.js
@@ -1,57 +1,53 @@
 import React, { useState } from "react";
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Button, ButtonGroup } from "react-bootstrap";
 import { BsBookmarkFill, BsPersonPlusFill } from "react-icons/bs";
 
-import './scss/app.scss';
-import Logo from './img/HarryPotter.png'
-import BtnFilter from './components/BtnFilter';
-import Card from './components/Card';
-import NewModal from './components/Modal';
+import "./scss/app.scss";
+import Logo from "./img/HarryPotter.png";
+import BtnFilter from "./components/BtnFilter";
+import Card from "./components/Card";
+import NewModal from "./components/Modal";
 
-const customData = require('./data/hp-characters.json');
-const students = require('./data/hp-students.json');
-const staff = require('./data/hp-staff.json');
+const characterData = require("./data/hp-characters.json");
+const students = require("./data/hp-students.json");
+const staff = require("./data/hp-staff.json");
+const customData = characterData.data;
 
 function App() {
-
-  const [view, setView] = useState('characters');
+  const [view, setView] = useState("characters");
   const [modalShow, setModalShow] = useState(false);
 
-  const handleView = (options) =>{
+  const handleView = (options) => {
     setView(options);
-  }
-  
+  };
+
   return (
-    <div className='App'>
-      <div className='header'>
-      <img src={Logo} className='logo' alt=''/>
-      <p className='firstText'>Selecciona tu filtro</p>
-      <BtnFilter handleView={handleView}/>
+    <div className="App">
+      <div className="header">
+        <img src={Logo} className="logo" alt="" />
+        <p className="firstText">Selecciona tu filtro</p>
+        <BtnFilter handleView={handleView} />
       </div>
       {(() => {
-        if (view === 'characters') {
-          return (
-            <Card customData={customData}/>
-          )
-        } else if (view === 'viewStudent') {
-          return (
-            <Card customData={students} />
-          )
+        if (view === "characters") {
+          return <Card customData={customData} />;
+        } else if (view === "viewStudent") {
+          return <Card customData={students} />;
         } else {
-          return (
-            <Card customData={staff} />
-          )
+          return <Card customData={staff} />;
         }
       })()}
-      <div className='btnOptions'>
-      <ButtonGroup >
-        <Button bsPrefix='btnPurpleDos'>FAVORITOS <BsBookmarkFill className='icons'/></Button>
-        <Button bsPrefix='btnPurpleDos' onClick={() => setModalShow(true)}>AGREGAR <BsPersonPlusFill className='icons'/></Button>
-        <NewModal show={modalShow} onHide={() => setModalShow(false)} />
-      </ButtonGroup>
-
+      <div className="btnOptions">
+        <ButtonGroup>
+          <Button bsPrefix="btnPurpleDos">
+            FAVORITOS <BsBookmarkFill className="icons" />
+          </Button>
+          <Button bsPrefix="btnPurpleDos" onClick={() => setModalShow(true)}>
+            AGREGAR <BsPersonPlusFill className="icons" />
+          </Button>
+          <NewModal show={modalShow} onHide={() => setModalShow(false)} />
+        </ButtonGroup>
       </div>
-      
     </div>
   );
 }
--- harrypotter/src/components/Modal.jsx
+++ harrypotter/src/components/Modal.jsx
@@ -1,81 +1,109 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import TextField from '@material-ui/core/TextField';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormLabel from '@material-ui/core/FormLabel';
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormLabel from "@material-ui/core/FormLabel";
 
-require('react-bootstrap/ModalHeader');
+require("react-bootstrap/ModalHeader");
 
 function NewModal(props) {
-    
   const [newCharacter, setNewCharacter] = useState({
-    name: '',
-    birthday: '',
-    eyesColor: '',
-    hairColor: '',
-    woman: '',
-    men: '',
-    student: '',
-    staff: '',
-    image: '',
+    name: "",
+    date: "",
+    eyesColor: "",
+    hairColor: "",
+    gender: "",
+    position: "",
   });
 
-  const handleInputChange = (e) => {
-    e.preventDefault();
+  const save = () => {
+    fetch("http://localhost:3001/data", {
+      method: "POST", // or 'PUT'
+      body: JSON.stringify(newCharacter), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error("Error:", error))
+      .then((response) => console.log("Success:", response));
+  };
+
+  const addCharacter = (e) => {
     setNewCharacter({
       ...newCharacter,
       [e.target.name]: e.target.value,
-      [e.target.birthday]: e.target.value,
+      [e.target.date]: e.target.value,
       [e.target.eyesColor]: e.target.value,
       [e.target.hairColor]: e.target.value,
-      [e.target.woman]: e.target.value,
-      [e.target.men]: e.target.value,
-      [e.target.student]: e.target.value,
-      [e.target.staff]: e.target.value,
-      [e.target.image]: e.target.files,
+      [e.target.gender]: e.target.value,
+      [e.target.position]: e.target.value,
     });
   };
 
   return (
-    <Modal
-      {...props}
-      centered
-    >
+    <Modal {...props} centered>
       <Modal.Header>
-        <Modal.Title id='modalTitle'>
-          Agregar un personaje
-        </Modal.Title>
+        <Modal.Title id="modalTitle">Agregar un personaje</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <form>
-          <div className='addContainer'>
-            <TextField className='addFact' id='filled-basic' label='Nombre' variant='filled' onChange={handleInputChange}/>
-            <TextField className='addFact' id='filled-basic' type='date'  variant='filled' onChange={handleInputChange}/>
-            <TextField className='addFact' id='filled-basic' label='Color de ojos' variant='filled' onChange={handleInputChange}/>
-            <TextField className='addFact' id='filled-basic' label='Color de pelo' variant='filled' onChange={handleInputChange}/>
-                <div className='radio'>
-                <FormLabel >GÉNERO</FormLabel>
-                <RadioGroup aria-label="gender" name="gender" onChange={handleInputChange}>
-                    <FormControlLabel value="woman" control={<Radio />} label="Mujer" />
-                    <FormControlLabel value="men" control={<Radio />} label="Hombre" />
-                </RadioGroup>
-                <FormLabel >POSICIÓN</FormLabel>
-                <RadioGroup aria-label="position" name="position" onChange={handleInputChange}>
-                    <FormControlLabel value="student" control={<Radio />} label="Estudiante" />
-                    <FormControlLabel value="staff" control={<Radio />} label="Staff" />
-                </RadioGroup>
-                </div>
-          </div>
-          <div className='btnFile'>
-            <label className='check' htmlFor='file'>FOTOGRAFÍA</label>
-            <input className='FileBtn' type='file' name='image' />
+          <div className="addContainer">
+            <TextField
+              className="addFact"
+              id="filled-basic"
+              label="Nombre"
+              variant="filled"
+              name="name"
+              onChange={addCharacter}
+            />
+            <TextField
+              className="addFact"
+              id="filled-basic"
+              type="date"
+              variant="filled"
+              name="date"
+              onChange={addCharacter}
+            />
+            <TextField
+              className="addFact"
+              id="filled-basic"
+              label="Color de ojos"
+              name="eyesColor"
+              variant="filled"
+              onChange={addCharacter}
+            />
+            <TextField
+              className="addFact"
+              id="filled-basic"
+              label="Color de pelo"
+              name="hairColor"
+              variant="filled"
+              onChange={addCharacter}
+            />
+            <div className="radio">
+              <FormLabel>GÉNERO</FormLabel>
+              <RadioGroup name="gender" onChange={addCharacter}>
+                <Radio value="woman" /> Mujer
+                <Radio value="men" /> Hombre
+              </RadioGroup>
+              <FormLabel>POSICIÓN</FormLabel>
+              <RadioGroup name="position" onChange={addCharacter}>
+                <Radio value="student" /> Estudiante
+                <Radio value="staff" /> Staff
+              </RadioGroup>
+            </div>
           </div>
         </form>
       </Modal.Body>
       <Modal.Footer>
-        <Button className='save' onClick={props.onHide}>
+        <Button
+          className="save"
+          onClick={() => {
+            save();
+          }}
+        >
           GUARDAR
         </Button>
       </Modal.Footer>
